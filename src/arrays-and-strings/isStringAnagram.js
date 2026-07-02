76f070117e012d057d8869b80bb2ac2c47131d60
Cameron Nokes <ccnokes@gmail.com>
2026-07-01 21:55:01 -0600

    update an old one

--- src/arrays-and-strings/isStringAnagram.js
+++ src/arrays-and-strings/isStringAnagram.js
@@ -1,16 +1,14 @@
-// @ts-check
-
 /**
  * Given two strings str1 and str2, determine if str2 is an anagram of str1 and return true if it is, and false otherwise.
- * An anagram is a word or phrase formed by rearranging the letters of another word or phrase, using all the original 
- * letters exactly once. For example, the words listen and silent are anagrams because they use the same letters with 
+ * An anagram is a word or phrase formed by rearranging the letters of another word or phrase, using all the original
+ * letters exactly once. For example, the words listen and silent are anagrams because they use the same letters with
  * the same frequency, but in a different order.
- * 
+ *
  * @param {string} str1
  * @param {string} str2
  * @return {boolean}
  */
-export default function isStringAnagram(str1, str2) {
+export function isStringAnagram_old(str1, str2) {
   // if strings are different lengths, return false
   if (str1.length !== str2.length) {
     return false;
@@ -21,14 +19,14 @@ export default function isStringAnagram(str1, str2) {
   const str1CharCount = new Map();
   const str2CharCount = new Map();
 
-  for(let i = 0; i < str1.length; i++) {
+  for (let i = 0; i < str1.length; i++) {
     const str1Char = str1[i];
     const str2Char = str2[i];
     str1CharCount.set(str1Char, (str1CharCount.get(str1Char) ?? 0) + 1);
     str2CharCount.set(str2Char, (str2CharCount.get(str2Char) ?? 0) + 1);
   }
 
-  for(const key of str1CharCount.keys()) {
+  for (const key of str1CharCount.keys()) {
     if (str1CharCount.get(key) !== str2CharCount.get(key)) {
       return false;
     }
@@ -43,15 +41,14 @@ export default function isStringAnagram(str1, str2) {
 // iterate maps, check each char frequency is same
 // O(n)
 
-
 /**
-* O(n log n) solution that's easier
-* 
-* @param {string} str1
-* @param {string} str2
-* @return {boolean}
-*/
-function isStringAnagramEasy(str1, str2) {
+ * O(n log n) solution (because of sort) that's easy
+ *
+ * @param {string} str1
+ * @param {string} str2
+ * @return {boolean}
+ */
+function isStringAnagramWithArraySort(str1, str2) {
   // if strings are different lengths, return false
   if (str1.length !== str2.length) {
     return false;
@@ -62,4 +59,31 @@ function isStringAnagramEasy(str1, str2) {
   const str2Sorted = Array.from(str2).sort().join('');
 
   return str1Sorted === str2Sorted;
+}
+
+// like above but more efficient because we add/subtract from a map
+// and the check all the values of the map are 0 at the end
+// O(n)
+export function isStringAnagram(str1, str2) {
+  // if strings are different lengths, return false
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  const charCounts = new Map();
+
+  for (let i = 0; i < str1.length; i++) {
+    const str1Char = str1[i];
+    charCounts.set(str1Char, (charCounts.get(str1Char) ?? 0) + 1);
+    const str2Char = str2[i];
+    charCounts.set(str2Char, (charCounts.get(str2Char) ?? 0) - 1);
+  }
+
+  for (const value of charCounts.values()) {
+    if (value !== 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
--- src/arrays-and-strings/test.js
+++ src/arrays-and-strings/test.js
@@ -2,20 +2,20 @@
 import test from 'ava';
 import romanNumeralToInt from './romanNumeralToInt.js';
 import productExceptSelf from './productExceptSelf.js';
-import isStringAnagram from './isStringAnagram.js';
+import { isStringAnagram } from './isStringAnagram.js';
 
-test('romanNumeralToInt', t => {
+test('romanNumeralToInt', (t) => {
   t.is(romanNumeralToInt('XIII'), 13);
   t.is(romanNumeralToInt('IV'), 4);
   t.is(romanNumeralToInt('MCMXCIV'), 1994);
   t.is(romanNumeralToInt('LVIII'), 58);
 });
 
-test('productExceptSelf', t => {
+test('productExceptSelf', (t) => {
   t.deepEqual(productExceptSelf([1, 2, 3, 4]), [24, 12, 8, 6]);
 });
 
-test('isStringAnagram', t => {
+test('isStringAnagram', (t) => {
   t.is(isStringAnagram('hello', 'bello'), false);
   t.is(isStringAnagram('listen', 'silent'), true);
   t.is(isStringAnagram('abc', 'bcaa'), false);
